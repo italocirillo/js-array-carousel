57ed99a8589f7db41533638d48b72ea0c5e57593
italocirillo <italocirillo@gmail.com>
2023-03-14 18:42:24 +0100

    arrey immagini aggiunte

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("ciao");
+// INIZIALIZZAZIONE
+const arrayImmagini = ["img/01.jpg","img/02.jpg","img/03.jpg","img/04.jpg","img/05.jpg"];
+
+const slider = document.querySelector(".slider");
+
+// LOGICA
+// Ciclo for che crea le varie immagini
+for(let i=0;i<arrayImmagini.length;i++){
+    const Immagine = ` <img class="elemento-immagine" src="${arrayImmagini[i]}" alt="img1"> `;
+    slider.innerHTML += Immagine;
+}
